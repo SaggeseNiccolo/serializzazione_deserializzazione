41c95880849ec0e6018af17d9704c4e3d758c108
lai2-01 <lai2-01@itismeucci.com>
2022-01-12 13:10:10 +0100

    creato progetto maven

---
+++ deserializzazione_xml/src/main/java/com/saggese/App.java
@@ -0,0 +1,13 @@
+package com.saggese;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
