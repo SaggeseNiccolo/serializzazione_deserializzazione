9bdf1f66089561bebfcb5ea778ae0a63ca9989ac
lai2-01 <lai2-01@itismeucci.com>
2022-01-12 13:17:35 +0100

    aggiunto xmlmapper

--- deserializzazione_xml/src/main/java/com/saggese/App.java
+++ deserializzazione_xml/src/main/java/com/saggese/App.java
@@ -1,13 +1,9 @@
 package com.saggese;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    XmlMapper mapper = new XmlMapper();
+
+    public static void main(String[] args) {
     }
 }
